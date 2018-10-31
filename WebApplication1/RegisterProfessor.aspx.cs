f1f4a6239647b2fbeea537168a214d09c7cef59a
AYUSH GARG <ayush.garg1@learner.manipal.edu>
2018-10-31 21:06:28 +0530

    Connection to the database

--- WebApplication1/RegisterProfessor.aspx.cs
+++ WebApplication1/RegisterProfessor.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,7 +18,23 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
-
+            int id = int.Parse(TextBox1.Text);
+            string name = TextBox2.Text;
+            string subject = TextBox3.Text;
+            string pwd = TextBox4.Text;
+            string designation = DropDownList1.SelectedValue;
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.ConnectionString = @"Data Source=dellmac.database.windows.net;Initial Catalog=IT Lab Project;User ID=ayushgarg;Password=@dell123;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlCommand command = new SqlCommand("INSERT INTO Professors VALUES('" + id + "','" + name + "','" + designation + "','" + subject + "','" + pwd + "')", sqlConnection);
+            try
+            {
+                sqlConnection.Open();
+                command.ExecuteNonQuery();
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
         }
     }
 }
