d2c7168ced8551b2aae0a59f9513e79654b63ee2
Ayush Acharya <aka7acharya@gmail.com>
2018-11-01 21:11:54 +0530

    connection

--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -16,7 +17,13 @@ namespace WebApplication1
 
         protected void Button1_Click(object sender, EventArgs e)
         {
+            SqlDataReader reader = null;
+            SqlConnection sqlConnection = new SqlConnection();
+            sqlConnection.Open();
+            SqlCommand cmd = new SqlCommand("SELECT [Email_Id] from Professors where (Email_ID = ' " + TextBox2.Text + " ')";
 
+            sqlConnection.ConnectionString = @"Data Source=dellmac.database.windows.net;Initial Catalog=IT Lab Project;User ID=ayushgarg;Password=@dell123;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlCommand command = new SqlCommand("Select INTO Professors VALUES('" + id + "','" + name + "','" + designation + "','" + subject + "','" + pwd + "')", sqlConnection);
            
         }
     }
